b7524b798b12aa01263f2bc336a45879d0d39097
Tommy <tomjeanpop@gmail.com>
2026-07-18 21:22:47 -0400

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
+	output: 'server',
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
